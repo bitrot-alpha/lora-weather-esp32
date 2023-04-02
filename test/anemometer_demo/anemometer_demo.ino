641e54dc71945fff18934050e7b1bd4fca044d7d
bitrot-alpha <robertgleoto706@gmail.com>
2023-04-01 17:22:08 -0700

    Anemometer demo - shows Wind MPH now

--- test/anemometer_demo/anemometer_demo.ino
+++ test/anemometer_demo/anemometer_demo.ino
@@ -1,57 +1,81 @@
-/*  Author: Bitrot_alpha
+/*  Author: Bitrot_alpha, nseidle
  *  This is demo code for the anemometer.
  *    A quirk of the unit I'm using is that it double-switches for each pass of the magnet (reed switch sensor)..
  *    I've found this to happen AFTER using a RC filter for debouncing (it's slow enough to bypass the filter).
  *    The workaround I've applied is to divide the count value by 2.
+ *
+ *  Most of this is lifted from https://github.com/sparkfun/Wimp_Weather_Station (nseidle's code)
  */
 
 #include "Arduino.h"
 #include "HT_SSD1306Wire.h"
 
-struct Switch 
-{
-    const uint8_t PIN;
-    uint32_t numberKeyPresses;
-    bool pressed;
-};
+//anemometer pin
+const uint8_t WIND_SPD_PIN = 4;
+//if your anemometer is bouncy, set this to something other than 1 to "calibrate" it
+const uint8_t BOUNCE_COMPENSATION = 2;
 
-Switch switch1 = {47, 0, false};
+//display stuff
+SSD1306Wire oled(0x3c, 500000, SDA_OLED, SCL_OLED, GEOMETRY_128_64, RST_OLED); // addr , freq , i2c group , resolution , rst
+char revcount_str[20] = "";
+char windmph_str[20] = "";
+long prevDisplayTime = 0;
 
-void ARDUINO_ISR_ATTR isr(void* arg) 
+//globals for tracking wind
+long lastWindCheck = 0;
+volatile long lastWindIRQ = 0;
+volatile uint8_t windRevs = 0;
+
+
+void wind_spd_IRQ()
 {
-    Switch* s = static_cast<Switch*>(arg);
-    //disable interrupt to avoid repeat handling
-    detachInterrupt(s->PIN);
-    //flag our real interrupt handler
-    s->pressed = true;
+  if (millis() - lastWindIRQ > 10) // Ignore switch-bounce glitches less than 10ms (142MPH max reading) after the reed switch closes
+	{
+		lastWindIRQ = millis(); //Grab the current time
+		windRevs++; //There is 1.492MPH for each click per second.
+	}
 }
 
-void ARDUINO_ISR_ATTR isr() {}
+float get_wind_speed()
+{
+	float deltaTime = millis() - lastWindCheck;
 
-SSD1306Wire oled(0x3c, 500000, SDA_OLED, SCL_OLED, GEOMETRY_128_64, RST_OLED); // addr , freq , i2c group , resolution , rst
-char display_str[30] = "";
+	deltaTime /= 1000.0; //Convert to seconds
+
+	float windSpeed = (float)(windRevs / BOUNCE_COMPENSATION) / deltaTime;
+
+	windRevs = 0; //Reset and start watching for new wind
+	lastWindCheck = millis();
+
+	windSpeed *= 1.492;
+
+	return(windSpeed);
+}
 
 void setup()
 {
     //Serial.begin(115200);
-    pinMode(switch1.PIN, INPUT);
-    attachInterruptArg(switch1.PIN, isr, &switch1, FALLING);
+    pinMode(WIND_SPD_PIN, INPUT);
+    attachInterrupt(WIND_SPD_PIN, wind_spd_IRQ, FALLING);
     oled.init();
     oled.clear();
+    oled.setFont(ArialMT_Plain_16);
 }
 
 void loop() 
 {
-    if (switch1.pressed)
-    {
-      //clear flag
-      switch1.pressed = false;
-      switch1.numberKeyPresses += 1;
-      snprintf(display_str, 30, "Anemometer rev count: %u\0", (switch1.numberKeyPresses / 2) );
-      oled.clear();
-      oled.drawString(0, 0, display_str);
-      oled.display();
-      //re-enable interrupt
-      attachInterruptArg(switch1.PIN, isr, &switch1, FALLING);
-    }
+  long now = millis();
+
+  //wait 1 second between outputs to display
+  if(now - prevDisplayTime > 1000)
+  {
+    prevDisplayTime = now;
+
+    snprintf(revcount_str, 20, "Revs: %3u\0", (windRevs / BOUNCE_COMPENSATION) );
+    snprintf(windmph_str, 20, "Wind MPH: %4.2f\0", get_wind_speed() );
+    oled.clear();
+    oled.drawString(0, 0, revcount_str);
+    oled.drawString(0, 15, windmph_str);
+    oled.display();
+  }
 }
